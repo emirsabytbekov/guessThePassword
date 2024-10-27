a74de4dfb45d2a7e4f25d25a5196c807125bb07c
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2024-10-27 23:29:23 +0600

    fix other bugs

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -11,19 +12,34 @@ public class Main {
         ArrayList<Integer> guess = new ArrayList<>(arrayListSize);
 
         int p = 0;
+        int numberOfGreenNumbers = 0;
         ArrayList<Integer> isFoundOrNot = new ArrayList<>(arrayListSize);
+        ArrayList<Integer> isHasToBeYellowOrNot = new ArrayList<>(arrayListSize);
+
+
+        for (int i = 0; i < arrayListSize; i++) {
+            password.add(rand.nextInt(10));
+        }
 
 //        for (int i = 0; i < arrayListSize; i++) {
-//            password.add(rand.nextInt(10));
+//            password.add(sc.nextInt());
 //        }
-        for (int i = 0; i < arrayListSize; i++) {
-            password.add(sc.nextInt());
-        }
 
         for (int i = 0; i < arrayListSize; i++) {
             int element = sc.nextInt();
             guess.add(element);
         }
+
+        for (; p < arrayListSize; p++) {
+            if (guess.get(p) == password.get(p)) isFoundOrNot.add(1);
+            else isFoundOrNot.add(0);
+        }
+
+        for (int i = 0; i < arrayListSize; i++) {
+            isHasToBeYellowOrNot.add(0);
+        }
+
+
 
         for (int numberOfAttempts = 5; numberOfAttempts > 0; numberOfAttempts--)
         {
@@ -32,31 +48,59 @@ public class Main {
                     int element = sc.nextInt();
                     guess.set(i, element);
                 }
+
+                for (int i = 0; i < arrayListSize; i++) {
+                    if (guess.get(i) == password.get(i)) isFoundOrNot.set(i, 1);
+                    else isFoundOrNot.set(i, 0);
+                }
+
             }
 
             for (int i = 0; i < arrayListSize; i++) {
-                for (; p < arrayListSize; p++) {
-                    if (guess.get(p) == password.get(p)) isFoundOrNot.add(1);
-                    else isFoundOrNot.add(0);
+                isHasToBeYellowOrNot.set(i, 0);
+            }
+
+            for (int i = 0; i < arrayListSize; i++) {
+                for (int j = 0; j < arrayListSize; j++) {
+                    if (Objects.equals(guess.get(i), password.get(j)) && isFoundOrNot.get(j) == 0) {
+                        isHasToBeYellowOrNot.set(i, 1);
+                    }
                 }
+            }
+
+
+            numberOfGreenNumbers = numberOfGreenNumbers - numberOfGreenNumbers;
+
+            for (int i = 0; i < arrayListSize; i++) {
 
                 if (guess.get(i) == password.get(i)) {
-                    System.out.print("\n" + guess.get(i) + "\uD83D\uDFE2");
+                    System.out.print(guess.get(i) + "\uD83D\uDFE2 "); //green
+                    numberOfGreenNumbers++;
                 }
+
                 else
                 {
                     for (int j = 0; j < arrayListSize; j++) {
-                        if (guess.get(i) == password.get(j) && isFoundOrNot.get(j) == 0) {
-                            System.out.print("\n" + guess.get(i) + "\uD83D\uDFE1");
+                        if (Objects.equals(guess.get(i), password.get(j)) && isFoundOrNot.get(j) == 0) {
+                            System.out.print(guess.get(i) + "\uD83D\uDFE1 "); //yellow
                             break;
                         }
-                        else {
-                            System.out.print("\n" + guess.get(i) + "\uD83D\uDD34");
+                    }
+                    for (int j = 0; j < arrayListSize; j++) {
+                        if (isHasToBeYellowOrNot.get(i) == 0 && (!Objects.equals(guess.get(i), password.get(j)) || isFoundOrNot.get(j) == 1)) {
+                            System.out.print(guess.get(i) + "\uD83D\uDD34 "); //red
                             break;
                         }
                     }
                 }
+            }
+            System.out.println(" ");
 
+            if (numberOfGreenNumbers == arrayListSize)
+            {
+                System.out.println("You have successfully guessed the password!");
+                System.out.println("Congratulations!");
+                break;
             }
         }
 
