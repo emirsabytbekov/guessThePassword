9b442c605e7898377697bfec639dd56a326f5f04
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2024-10-28 14:56:34 +0600

    add methods, add Win message, add Lose message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
+    public static int numberOfGreenNumbers;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
@@ -11,102 +12,169 @@ public class Main {
         ArrayList<Integer> password = new ArrayList<>(arrayListSize);
         ArrayList<Integer> guess = new ArrayList<>(arrayListSize);
 
-        int p = 0;
-        int numberOfGreenNumbers = 0;
-        ArrayList<Integer> isFoundOrNot = new ArrayList<>(arrayListSize);
-        ArrayList<Integer> isHasToBeYellowOrNot = new ArrayList<>(arrayListSize);
+        ArrayList<Integer> isGreenOrNot = new ArrayList<>(arrayListSize);
+        ArrayList<Integer> isYellowOrNot = new ArrayList<>(arrayListSize);
 
 
-        for (int i = 0; i < arrayListSize; i++) {
-            password.add(rand.nextInt(10));
-        }
+        createPassword(arrayListSize, password, rand);
 
-//        for (int i = 0; i < arrayListSize; i++) {
-//            password.add(sc.nextInt());
-//        }
+//        setPassword(arrayListSize, password, sc);
 
-        for (int i = 0; i < arrayListSize; i++) {
-            int element = sc.nextInt();
-            guess.add(element);
-        }
+        getFirstGuess(arrayListSize, guess, sc);
 
-        for (; p < arrayListSize; p++) {
-            if (guess.get(p) == password.get(p)) isFoundOrNot.add(1);
-            else isFoundOrNot.add(0);
-        }
+        findGreenNumbers(arrayListSize, guess, password, isGreenOrNot);
 
-        for (int i = 0; i < arrayListSize; i++) {
-            isHasToBeYellowOrNot.add(0);
-        }
+        makeArrayListOfZeroes(arrayListSize, isYellowOrNot);
 
 
-
-        for (int numberOfAttempts = 5; numberOfAttempts > 0; numberOfAttempts--)
+        int numberOfAttemptsRemaining = 5; //the most optimal number of attempts
+        for (; numberOfAttemptsRemaining > 0; numberOfAttemptsRemaining--)
         {
-            if (numberOfAttempts < 5) {
-                for (int i = 0; i < arrayListSize; i++) {
-                    int element = sc.nextInt();
-                    guess.set(i, element);
-                }
-
-                for (int i = 0; i < arrayListSize; i++) {
-                    if (guess.get(i) == password.get(i)) isFoundOrNot.set(i, 1);
-                    else isFoundOrNot.set(i, 0);
-                }
-
+            if (numberOfAttemptsRemaining < 5) {
+                getNewGuess(arrayListSize, sc, guess);
+                getNewGreenNumbers(arrayListSize, guess, password, isGreenOrNot);
             }
 
-            for (int i = 0; i < arrayListSize; i++) {
-                isHasToBeYellowOrNot.set(i, 0);
-            }
+            getYellowNumbers(arrayListSize, isYellowOrNot);
 
-            for (int i = 0; i < arrayListSize; i++) {
-                for (int j = 0; j < arrayListSize; j++) {
-                    if (Objects.equals(guess.get(i), password.get(j)) && isFoundOrNot.get(j) == 0) {
-                        isHasToBeYellowOrNot.set(i, 1);
-                    }
-                }
-            }
+            getNewYellowNumbers(arrayListSize, guess, password, isGreenOrNot, isYellowOrNot);
 
 
-            numberOfGreenNumbers = numberOfGreenNumbers - numberOfGreenNumbers;
+            numberOfGreenNumbers = 0;
 
-            for (int i = 0; i < arrayListSize; i++) {
+            displayColoursOfNumbers(arrayListSize, guess, password, isGreenOrNot, isYellowOrNot);
 
-                if (guess.get(i) == password.get(i)) {
-                    System.out.print(guess.get(i) + "\uD83D\uDFE2 "); //green
-                    numberOfGreenNumbers++;
-                }
-
-                else
-                {
-                    for (int j = 0; j < arrayListSize; j++) {
-                        if (Objects.equals(guess.get(i), password.get(j)) && isFoundOrNot.get(j) == 0) {
-                            System.out.print(guess.get(i) + "\uD83D\uDFE1 "); //yellow
-                            break;
-                        }
-                    }
-                    for (int j = 0; j < arrayListSize; j++) {
-                        if (isHasToBeYellowOrNot.get(i) == 0 && (!Objects.equals(guess.get(i), password.get(j)) || isFoundOrNot.get(j) == 1)) {
-                            System.out.print(guess.get(i) + "\uD83D\uDD34 "); //red
-                            break;
-                        }
-                    }
-                }
-            }
             System.out.println(" ");
 
             if (numberOfGreenNumbers == arrayListSize)
             {
-                System.out.println("You have successfully guessed the password!");
-                System.out.println("Congratulations!");
+                System.out.println(printWinMessage());
                 break;
             }
         }
 
-
-
-        System.out.println(password);
-
+        if (numberOfAttemptsRemaining == 0) {
+            printLoseMessage(arrayListSize, password);
+        }
     }
+
+
+
+
+    public static void createPassword (int arrayListSize, ArrayList<Integer> password, Random rand) {
+        for (int i = 0; i < arrayListSize; i++) {
+            password.add(rand.nextInt(10));
+        }
+    }
+
+    public static void setPassword (int arrayListSize, ArrayList<Integer> password, Scanner sc) {
+        for (int i = 0; i < arrayListSize; i++) {
+            password.add(sc.nextInt());
+        }
+    }
+
+    public static void getFirstGuess(int arrayListSize, ArrayList<Integer> guess, Scanner sc) {
+        for (int i = 0; i < arrayListSize; i++) {
+            guess.add(sc.nextInt());
+        }
+    }
+
+    public static void findGreenNumbers (int arrayListSize, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+            if (Objects.equals(guess.get(i), password.get(i))) isGreenOrNot.add(1);
+            else isGreenOrNot.add(0);
+        }
+    }
+
+    public static void makeArrayListOfZeroes (int arrayListSize, ArrayList<Integer> isYellowOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+            isYellowOrNot.add(0);
+        }
+    }
+
+    public static void getNewGuess (int arrayListSize, Scanner sc, ArrayList<Integer> guess) {
+        for (int i = 0; i < arrayListSize; i++) {
+            int element = sc.nextInt();
+            guess.set(i, element);
+        }
+    }
+
+    public static void getNewGreenNumbers(int arrayListSize, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+            if (Objects.equals(guess.get(i), password.get(i))) isGreenOrNot.set(i, 1);
+            else isGreenOrNot.set(i, 0);
+        }
+    }
+
+    public static void getYellowNumbers (int arrayListSize, ArrayList<Integer> isYellowOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+            isYellowOrNot.set(i, 0);
+        }
+    }
+
+    public static void getNewYellowNumbers(int arrayListSize, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot, ArrayList<Integer> isYellowOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+            for (int j = 0; j < arrayListSize; j++) {
+                if (Objects.equals(guess.get(i), password.get(j)) && isGreenOrNot.get(j) == 0) {
+                    isYellowOrNot.set(i, 1);
+                }
+            }
+        }
+    }
+
+    public static void displayColoursOfNumbers (int arrayListSize, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot, ArrayList<Integer> isYellowOrNot) {
+        for (int i = 0; i < arrayListSize; i++) {
+
+            showFoundGreenNumbers(i, guess, password);
+
+            if (!Objects.equals(guess.get(i), password.get(i)))
+            {
+                showFoundYellowNumbers(arrayListSize, i, guess, password, isGreenOrNot);
+                showFoundRedNumbers(arrayListSize, i, isYellowOrNot, guess, password, isGreenOrNot);
+            }
+        }
+    }
+
+    public static void showFoundGreenNumbers(int i, ArrayList<Integer> guess, ArrayList<Integer> password) {
+        if (Objects.equals(guess.get(i), password.get(i))) {
+            System.out.print(guess.get(i) + "\uD83D\uDFE2 "); //green
+            numberOfGreenNumbers++;
+        }
+    }
+
+    public static void showFoundYellowNumbers (int arrayListSize, int i, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot) {
+        for (int j = 0; j < arrayListSize; j++) {
+            if (Objects.equals(guess.get(i), password.get(j)) && isGreenOrNot.get(j) == 0) {
+                System.out.print(guess.get(i) + "\uD83D\uDFE1 "); //yellow
+                break;
+            }
+        }
+    }
+
+    public static void showFoundRedNumbers (int arrayListSize, int i, ArrayList<Integer> isYellowOrNot, ArrayList<Integer> guess, ArrayList<Integer> password, ArrayList<Integer> isGreenOrNot) {
+        for (int j = 0; j < arrayListSize; j++) {
+            if (isYellowOrNot.get(i) == 0 && (!Objects.equals(guess.get(i), password.get(j)) || isGreenOrNot.get(j) == 1)) {
+                System.out.print(guess.get(i) + "\uD83D\uDD34 "); //red
+                break;
+            }
+        }
+    }
+
+    public static String printWinMessage() {
+        return "\nYou have successfully guessed the password!\nCongratulations!";
+    }
+
+    public static void printLoseMessage(int arrayListSize, ArrayList<Integer> password) {
+        System.out.print("\nYou have no attempts left.\nYou lost.\nThe password was: [");
+        printPassword(arrayListSize, password);
+        System.out.println("]");
+    }
+
+    public static void printPassword(int arrayListSize, ArrayList<Integer> password) {
+        for (int i = 0; i < arrayListSize; i++) {
+            if (i < arrayListSize - 1) System.out.print(password.get(i) + " ");
+            else System.out.print(password.get(i));
+        }
+    }
+
 }
